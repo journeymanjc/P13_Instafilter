79a114ef848a7905b8a5c1df6ce52691922d71be
Jae Cho <mobilewiz@gmail.com>
2022-06-12 22:01:29 -0400

    Initial Commit

---
+++ P13_Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P13_Instafilter
+//
+//  Created by Jae Cho on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P13_Instafilter/P13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  P13_InstafilterApp.swift
+//  P13_Instafilter
+//
+//  Created by Jae Cho on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct P13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
