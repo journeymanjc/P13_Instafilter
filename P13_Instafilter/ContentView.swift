dcded157d5753f5ab4d0c2d8592045f887cd6952
Jae Cho <mobilewiz@gmail.com>
2022-06-12 22:07:06 -0400

    Day62: Using property wrappers

--- P13_Instafilter/ContentView.swift
+++ P13_Instafilter/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State private var blurAmount = 0.0
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 VStack{
+			 Text("Hello, World!")
+				 .blur(radius: blurAmount)
+			 Slider(value: $blurAmount, in: 0...20)
+			 Button("Randmon Blur") {
+				 blurAmount = Double.random(in: 0...20)
+			 }
+		 }
     }
 }
 
