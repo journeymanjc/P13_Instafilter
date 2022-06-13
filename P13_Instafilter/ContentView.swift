261fbdf4e1a1552ab66a41044fb6d35cb4f1e11a
Jae Cho <mobilewiz@gmail.com>
2022-06-13 16:45:40 -0400

    Day62 Using .onChange() to act as didSet.

--- P13_Instafilter/ContentView.swift
+++ P13_Instafilter/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	@State private var blurAmount = 0.0
+	@State private var blurAmount = 0.0 {
+		didSet{
+			//DOES NOT WORK
+			print("New value is \(blurAmount)")
+		}
+	}
 	
     var body: some View {
 		 VStack{
 			 Text("Hello, World!")
 				 .blur(radius: blurAmount)
 			 Slider(value: $blurAmount, in: 0...20)
+				 .onChange(of: blurAmount) { newValue in
+					 //WORKS!
+					 print("New value is \(newValue)")
+				 }
 			 Button("Randmon Blur") {
 				 blurAmount = Double.random(in: 0...20)
 			 }
